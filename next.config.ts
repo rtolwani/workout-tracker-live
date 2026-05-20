b31432ab7f1cbb8a2de11e6a2b0a0991bc229f84
rtolwani <ravi@tolwani.com>
2026-05-20 12:11:56 -0700

    Fix next.config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/workout-tracker-live",
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
