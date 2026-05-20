7f7c1b50115c83936f9799af14e41e80cbedf51c
rtolwani <ravi@tolwani.com>
2026-05-20 12:10:00 -0700

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/workout-tracker-live",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
